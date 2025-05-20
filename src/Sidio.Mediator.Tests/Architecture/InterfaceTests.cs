7cf05ad389c2b87a3f4ca0484cbe8ff00786947f
Marthijn van den Heuvel <marthijn@sidio.nl>
2025-05-20 13:40:16 +0200

    ✅ Add architecture tests for interface naming conventions

---
+++ src/Sidio.Mediator.Tests/Architecture/InterfaceTests.cs
@@ -0,0 +1,21 @@
+﻿using ArchUnitNET.Loader;
+using ArchUnitNET.xUnit;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace Sidio.Mediator.Tests.Architecture;
+
+public sealed class InterfaceTests
+{
+    private static readonly ArchUnitNET.Domain.Architecture Architecture =
+        new ArchLoader().LoadAssemblies(typeof(IRequest).Assembly).Build();
+
+    [Fact]
+    public void Interfaces_ShouldStartWithI()
+    {
+        // Act
+        var rule = Interfaces().Should().HaveNameStartingWith("I");
+
+        // Assert
+        rule.Check(Architecture);
+    }
+}
