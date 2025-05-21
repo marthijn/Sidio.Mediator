b5ba844cd69de855475bc266ab3e617c1da9e75c
Marthijn van den Heuvel <marthijn@sidio.nl>
2025-05-21 09:36:35 +0200

    ✅ Add unit tests for Result class

---
+++ src/Sidio.Mediator.Tests/ResultTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Sidio.Mediator.Tests;
+
+public sealed class ResultTests
+{
+    private readonly Fixture _fixture = new ();
+
+    [Fact]
+    public void Success_ReturnsSuccessResult()
+    {
+        // Act
+        var result = Result.Success();
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.ValidationErrors.Should().BeEmpty();
+        result.ErrorCode.Should().BeNull();
+        result.ErrorMessage.Should().BeNull();
+    }
+
+    [Fact]
+    public void Failure_WithValidationErrors_ReturnsFailureResultWithValidationErrors()
+    {
+        // Arrange
+        var validationErrors = _fixture.CreateMany<ValidationError>().ToList();
+
+        // Act
+        var result = Result.Failure(validationErrors);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.ValidationErrors.Should().BeEquivalentTo(validationErrors);
+        result.ErrorCode.Should().BeNull();
+        result.ErrorMessage.Should().BeNull();
+    }
+
+    [Fact]
+    public void Failure_WithErrorCodeAndMessage_ReturnsFailureResultWithCodeAndMessage()
+    {
+        // Arrange
+        var errorCode = _fixture.Create<string>();
+        var errorMessage = _fixture.Create<string>();
+
+        // Act
+        var result = Result.Failure(errorCode, errorMessage);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.ValidationErrors.Should().BeEmpty();
+        result.ErrorCode.Should().Be(errorCode);
+        result.ErrorMessage.Should().Be(errorMessage);
+    }
+}
